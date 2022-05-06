7dd654de6db7e6fbd9dd2b6caea116eff0e10666
Jorge Aguilera <jorge.aguilera@seqera.io>
2022-05-06 14:58:26 +0200

    feature: add support for label/tags
    closes #2845
    
    Signed-off-by: Jorge Aguilera <jorge.aguilera@seqera.io>

--- modules/nextflow/src/main/groovy/nextflow/processor/TaskConfig.groovy
+++ modules/nextflow/src/main/groovy/nextflow/processor/TaskConfig.groovy
@@ -440,6 +440,10 @@ class TaskConfig extends LazyMap implements Cloneable {
         return CmdLineOptionMap.emptyOption()
     }
 
+    Map<String, String> getStickers() {
+        return (get('sticker') ?: Collections.emptyMap()) as Map<String, String>
+    }
+
     /**
      * Get a closure guard condition and evaluate to a boolean result
      *
